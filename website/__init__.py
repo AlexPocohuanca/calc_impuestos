b3b4be4dbf21c9358b269d19091f67e31871b205
Midna <greatprincessmidna@gmail.com>
2022-08-23 16:44:26 -0500

    1st dep

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'oihugyutrxfcvhlugyiftufchvzxcxzcxzcla'
+    app.config['SECRET_KEY'] = 'iftufchvzxcxzcxzcla'
     from .views import views
     app.register_blueprint(views, url_prefix='/')
     return app
