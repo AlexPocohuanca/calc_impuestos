66c2cbfef337075618e74575410518b98f001526
Midna <greatprincessmidna@gmail.com>
2022-08-23 16:30:11 -0500

    1st dep

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from website import create_app
 import os
-#app = create_app()
+app = create_app()
 
 # port = int(os.getenv('PORT'))
 
-#if __name__ == '__main__':
-#    try:
-#        app.run(debug=True) #, port = port)
-#    finally:
-#        pass
-
-
-from flask import Flask
-app = Flask(__name__)
-
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    try:
+        app.run(debug=True) #, port = port)
+    finally:
+        pass
+
+
         
